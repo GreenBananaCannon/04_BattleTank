bd01398db7f38224d7fa7ccd3545c482393402d8
Adam Tabis <atabs999@gmail.com>
2018-11-05 20:02:44 +0100

    Aiming has been fixed, but not refactored some logs removed

--- BattleTank/Source/BattleTank/Private/Tank.cpp
+++ BattleTank/Source/BattleTank/Private/Tank.cpp
@@ -11,16 +11,13 @@ ATank::ATank()
 {
  	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = false;
-	auto TankName = GetName();
-	UE_LOG(LogTemp, Warning, TEXT("GBC::%s Contructor in cpp"), *TankName);
 }
 
 // Called when the game starts or when spawned
 void ATank::BeginPlay()
 {
 	Super::BeginPlay(); // needed for BP beginplay to run!!!
-	auto TankName = GetName();
-	UE_LOG(LogTemp, Warning, TEXT("GBC::%s Begin play in cpp"), *TankName);
+	TankAimingComponent = FindComponentByClass<UTankAimingComponent>();
 }
 
 void ATank::AimAt(FVector HitLocation)
