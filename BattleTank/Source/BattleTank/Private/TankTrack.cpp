14106aef20d74ba19a44cd7df835ee2052c72ee6
Adam Tabis <atabs999@gmail.com>
2018-11-06 21:54:07 +0100

    Implemented functionality to negate tank slippage during movement

--- BattleTank/Source/BattleTank/Private/TankTrack.cpp
+++ BattleTank/Source/BattleTank/Private/TankTrack.cpp
@@ -2,7 +2,27 @@
 
 #include "TankTrack.h"
 
+UTankTrack::UTankTrack()
+{
+	PrimaryComponentTick.bCanEverTick = true;
+}
 
+void UTankTrack::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	auto TrackVelocity = GetComponentVelocity();
+	auto TrackRightVector = GetRightVector().GetSafeNormal();
+	auto SlippageSpeed = FVector::DotProduct(TrackRightVector, TrackVelocity);
+
+	//work out the required acceleration this frame to correct
+	//apply force in the opposite direction that we are slipping
+	auto CorrectionAcceleration = -SlippageSpeed / DeltaTime * TrackRightVector;
+	// calculate  and apply sidways force F = m * a
+	auto TankRoot = Cast<UStaticMeshComponent>(GetOwner()->GetRootComponent());
+	auto CorrectionForce = (TankRoot->GetMass() * CorrectionAcceleration) / 2; // two tracks 
+	TankRoot->AddForce(CorrectionForce);
+}
 
 void UTankTrack::SetThrottle(float Throttle)
 {
