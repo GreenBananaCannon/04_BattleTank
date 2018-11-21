5aa555dcdaeb3f1323318b1c381866041936bc09
Adam Tabis <atabs999@gmail.com>
2018-11-21 22:52:21 +0100

    Added wheels to tank treads, set up physics constraint programmatically

--- BattleTank/Source/BattleTank/Private/SprungWheel.cpp
+++ BattleTank/Source/BattleTank/Private/SprungWheel.cpp
@@ -13,36 +13,36 @@ ASprungWheel::ASprungWheel()
 
 	MassWheelConstraint = CreateDefaultSubobject<UPhysicsConstraintComponent>(FName("Mass Wheel Constraint"));
 	SetRootComponent(MassWheelConstraint);
-	
-
-	Mass = CreateDefaultSubobject<UStaticMeshComponent>(FName("Mass"));
-	Mass->SetupAttachment(MassWheelConstraint);
 
 	Wheel = CreateDefaultSubobject<UStaticMeshComponent>(FName("Wheel"));
 	Wheel->SetupAttachment(MassWheelConstraint);
 
+	
 }
 
 // Called when the game starts or when spawned
 void ASprungWheel::BeginPlay()
 {
 	Super::BeginPlay();
-	if (GetAttachParentActor())
-	{
-		UE_LOG(LogTemp, Warning, TEXT("Not Null: %s"), *GetAttachParentActor()->GetName())
-	}
-	else
-	{
-		UE_LOG(LogTemp, Warning, TEXT("Null"))
+	UE_LOG(LogTemp, Warning, TEXT("BeforeSetupConstraint"))
+	SetupConstraint();
+	UE_LOG(LogTemp, Warning, TEXT("AfterSetupConstraint"))
 
-	}
-	
 }
 
 // Called every frame
 void ASprungWheel::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
+}
 
+void ASprungWheel::SetupConstraint()
+{
+	UE_LOG(LogTemp, Warning, TEXT("EnteringSetupConstraint"))
+
+	if (!GetAttachParentActor()) { return; }
+	UPrimitiveComponent* BodyRoot = Cast<UPrimitiveComponent>(GetAttachParentActor()->GetRootComponent());
+	if (!BodyRoot) { return; }
+	MassWheelConstraint->SetConstrainedComponents(BodyRoot, NAME_None, Wheel, NAME_None);
 }
 
